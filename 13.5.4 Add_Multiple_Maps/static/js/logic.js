79f31d46583cb1807cdf1777b620b29a363e24ac
Durza75 <estrobach@gwu.edu>
2021-11-24 15:21:48 -0500

    initial 13.5.4

--- 13.5.4 Add_Multiple_Maps/static/js/logic.js
+++ 13.5.4 Add_Multiple_Maps/static/js/logic.js
@@ -1,4 +1,4 @@
-// 13.5.3 Map Multiple GeoJSON Points
+// 13.5.4 Add Multiple Maps
 // Add console.log to check to see if our code is working.
 console.log("working");
 console.log(API_KEY)
@@ -36,6 +36,29 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the airport GeoJSON URL
