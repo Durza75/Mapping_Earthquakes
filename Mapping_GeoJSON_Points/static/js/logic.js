581f47684b42e975b796837ec3a86e4adb05f70e
Durza75 <estrobach@gwu.edu>
2021-11-24 09:30:14 -0500

    mapping lines

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,24 +1,54 @@
-// 13.4.3 Map Lines
+// 13.5.2 Map GeoJSON Points
 // Add console.log to check to see if our code is working.
 console.log("working");
 console.log(API_KEY)
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 var accessToken = API_KEY;
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.47899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow",
-    mapStyle: "satellite-streets-v11"
-  }).addTo(map);
+
+
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+  }
+}).addTo(map);
+
+
+// // ** PER INSTRUCTIONS BUT DOES NOT WORK **
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng);
+//     .bindPopup("<h2>" + feature.properties.city + "</h2>");
+//   }
+
+// }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
